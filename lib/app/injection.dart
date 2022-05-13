cc8879cc28ad99344778df8fa00b8a4e55acc5cd
emerson-almeida <eemerson.almeida@gmail.com>
2022-05-13 06:47:52 -0300

    alterações no código Agenda

--- lib/app/injection.dart
+++ lib/app/injection.dart
@@ -1,14 +1,13 @@
 import 'package:agenda_crud/app/database/mysql/contact_dado_mysql.dart';
 import 'package:agenda_crud/app/domain/interfaces/contact_dao.dart';
 import 'package:agenda_crud/app/domain/services/contact_service.dart';
-import 'database/sqlite/dao/contact_dao_impl.dart';
+//import 'database/sqlite/dao/contact_dao_impl.dart';
 import 'package:flutter/material.dart';
 import 'package:get_it/get_it.dart';
 
-
-setupInjection() async{
+setupInjection() async {
   GetIt getIt = GetIt.I;
-  
+
   WidgetsFlutterBinding.ensureInitialized();
   //await Firebase.initializeApp();
   getIt.registerSingleton<ContactDAO>(ContactDAOMySQL());
